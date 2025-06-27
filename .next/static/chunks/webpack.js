edeb99e77e54284e5c0af398c949adcfa4315226
Ashwini Kumar <99595869+Ash0w0@users.noreply.github.com>
2025-06-27 20:14:54 +0530

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27e66fe7bba9e0af"; }
+/******/ 		__webpack_require__.h = function() { return "e4880db0db6b9b10"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
