1a8ad5ac613c7b6143a0308cc41e98eab62cecc2
Ashwini Kumar <99595869+Ash0w0@users.noreply.github.com>
2025-06-27 20:16:59 +0530

    Updated build-manifest.json

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -33,6 +33,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/login.js"
+    ],
+    "/tenant": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/tenant.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be7b96ea2eb355bc"; }
+/******/ 		__webpack_require__.h = function() { return "5955ed13fb6bb032"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
